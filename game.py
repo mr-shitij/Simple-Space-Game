c23a524448a23bd4694e0b40dcb63920a6bfc7a2
21shitijagrawal <21.shitijagrawal@gmail.com>
2020-06-11 14:53:25 +0530

    .

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import random
+import requests
 from time import sleep
+
 pygame.init()
 
 display_width = 1280
@@ -23,6 +25,8 @@ wilan = pygame.image.load('./asset/evilShip.png')
 blast = pygame.image.load('./asset/blast.png')
 
 score = 0
+hero_life = 100
+
 
 bullet_width = 2
 bullet_height = 20
@@ -71,6 +75,11 @@ def display_score():
 	textsurface = myfont.render('Score : ' + str(score), False, (255, 255, 255))
 	screen.blit(textsurface,(0, 0))
 
+def display_hero_life():
+	myfont = pygame.font.SysFont('Comic Sans MS', 50)
+	textsurface = myfont.render('Life : ' + str(hero_life), False, (255, 255, 255))
+	screen.blit(textsurface,(int(display_width / 2), 0))
+
 
 def draw_border():
 	pygame.draw.rect(screen, (255, 0, 0), [border_startX, border_startY, border_width, border_height], 2)
@@ -89,6 +98,19 @@ def mov_hero_to_right():
 	if not ((hero_positionX + hero.get_rect().size[0]) + displacement) > (border_startX + border_width):
 		hero_positionX = hero_positionX + displacement
 
+def mov_hero_to_up():
+	displacement = 15
+	global hero_positionY
+	global border_startY
+	if not (hero_positionY - displacement) < border_startX:
+		hero_positionY = hero_positionY - displacement
+
+def mov_hero_to_down():
+	displacement = 15
+	global hero_positionY
+	global border_startY
+	if not ((hero_positionY + hero.get_rect().size[1]) + displacement) > (border_startY + border_height):
+		hero_positionY = hero_positionY + displacement
 
 def fire():
 	play_fire_audio()
@@ -112,12 +134,34 @@ def destory_old_wilans():
 		except:
 			pass
 
+def game_Over():
+	for img in range(1, 9, 1):
+		screen.blit(pygame.image.load('./asset/blast'+ str(img) +'.png'), (int(hero_positionX), int(hero_positionY)))
+		pygame.display.update()
+		play_blast_audio()
+		sleep(1)
+
+	screen.blit(pygame.transform.scale(pygame.image.load('./asset/gameOver.png'), (int(border_width), int(border_height /2))), (border_startX, border_startY / 2))
+	pygame.display.update()
+	sleep(5)
+
 def display_wilans():
+	global game_over
 	destory_old_wilans()	
 	while len(wilans_positions[0]) < total_wilan:
 		add_new_wilan()
 		
 	for pos in range(len(wilans_positions[0])):
+		if (wilans_positions[1][pos] >= hero_positionY and wilans_positions[1][pos] <= hero_positionY + hero.get_rect().size[1]) or (wilans_positions[1][pos]  + wilan.get_rect().size[1] >= hero_positionY and wilans_positions[1][pos]  + wilan.get_rect().size[1] <= hero_positionY + hero.get_rect().size[1]) and game_over == False:
+			if (wilans_positions[0][pos] >= (hero_positionX + 10) and wilans_positions[0][pos] <= (hero_positionX + hero.get_rect().size[0]) - 10) or (wilans_positions[0][pos]  + wilan.get_rect().size[0] >= (hero_positionX + 10) and wilans_positions[0][pos]  + wilan.get_rect().size[0] <= (hero_positionX + hero.get_rect().size[0]) - 10):
+				global hero_life
+				if hero_life == 0:
+					game_over = True
+					break
+				else:	
+					hero_life -= 1
+		if game_over == True:
+			break
 		wilans_positions[1][pos] += wilans_speed[pos]
 		screen.blit(wilan, (wilans_positions[0][pos], wilans_positions[1][pos]))
 
@@ -131,9 +175,9 @@ def process_bullets():
 			try:
 				if wilans_positions[0][position] <= bullet[0] and wilans_positions[0][position] + wilan.get_rect().size[0] >= bullet[0]:
 					if (wilans_positions[1][position] <= bullet[1] and wilans_positions[1][position] + wilan.get_rect().size[1] >= bullet[1]):
+						play_blast_audio()
 						show_animation(wilans_positions[0][position], wilans_positions[1][position])
 						score = score + 1
-						play_blast_audio()
 						bullets.remove(bullet)
 						del wilans_positions[0][position]
 						del wilans_positions[1][position]
@@ -155,10 +199,15 @@ def process_bullets():
 def update():
 	screen.blit(background, (0, 0))
 	process_bullets()
+	screen.blit(hero, (hero_positionX, hero_positionY))
 	display_wilans()
 	draw_border()
 	display_score()
-	screen.blit(hero, (hero_positionX, hero_positionY))
+	display_hero_life()
+
+def get_data():
+	
+	return int(requests.get("http://192.168.1.103").text)
 
 def init():
 	pygame.mixer.init()
@@ -176,16 +225,24 @@ while not game_over:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game_over = True
-			
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				mov_hero_to_left()
-			if event.key == pygame.K_RIGHT:
-				mov_hero_to_right()
-			if event.key == pygame.K_SPACE:
-				fire()
-	sleep(0.02)
+			pygame.quit()
+
+	data = get_data()
+	print(data)
+	if data == 254:
+		mov_hero_to_right()
+	elif data == 253:
+		mov_hero_to_left()
+	elif data == 247 or data == 246:
+		mov_hero_to_up()
+	elif data == 251:
+		mov_hero_to_down()
+	elif data == 127:
+		fire()
+
 	update()
 	pygame.display.update()
+
+game_Over()
 
 pygame.quit()
